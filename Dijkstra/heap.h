663bb5208e679458b6e5e8d7453298fe29d839b1
Rennan Weslley <rennanweslley18@gmail.com>
2019-11-12 15:29:42 -0300

    Min priority q

---
+++ Dijkstra/heap.h
@@ -0,0 +1,179 @@
+#ifndef HEAP_H
+#define HEAP_H
+
+#include <iostream>
+#include <sstream>
+
+class Heap {
+    private:
+        int *data;
+        int size;
+
+    public:
+        Heap(int *data, int size) {
+            this->data = data;
+            this->size = size;
+        }
+        
+        int getParent(int i) {
+            return i/2;
+        }
+        
+        int getLeft(int i) {
+            return 2*i;
+        }
+
+        int getRight(int i) {
+            return 2*i + 1;
+        }
+        
+        bool isEmpty() {
+            return size == 0;
+        }
+        
+        int getSize() {
+            return size;
+        }
+        
+        int *getData() {
+            return data;
+        }
+        
+        void min_heapify(int i) {
+            if(this->isEmpty())
+                return;
+            
+            int l = this->getLeft(i),
+                r = this->getRight(i),
+                u;
+            
+            if(l <= this->getSize() && this->getData()[l-1] < this->getData()[i-1])
+                u = l;
+            else
+                u = i;
+            
+            if(r <= this->getSize() && this->getData()[r-1] < this->getData()[u-1])
+                u = r;
+            
+            if(u != i) {
+                std::swap(this->getData()[i-1], this->getData()[u-1]);
+                
+                min_heapify(u);
+            }
+        }
+        
+        void sizeDec() {
+            size--;
+        }
+        
+        int heap_minimum() {
+            return data[0];
+        }
+        
+        int extract_min() {
+            int aux = data[0];
+        
+            data[0] = data[size-1];
+            
+            size--;
+                
+            min_heapify(1);
+            
+            return aux;
+        }
+        
+        void decrease_key(int i, int key) {
+            i--;
+            
+            if(data[i] < key)
+                return;
+            
+            std::cout << this->toString() << "\n" << std::endl;
+            
+            data[i] = key;
+            
+            for(; i && (data[this->getParent(i)] > data[i]); i = this->getParent(i))
+                std::swap(data[this->getParent(i)], data[i]);
+            
+            std::cout << this->toString() << "\n" << std::endl;
+        }
+        
+        std::string toString() {
+            if(this->isEmpty())
+                return "Heap is empty\n";
+            
+            std::stringstream d, s;
+            std::string show = "Heap.: {";
+            
+            for(int i = 0; i < size; i++) {
+                d << data[i];
+                
+                if((i+1) == size) {
+                    show += d.str() + "}";
+                    break;
+                }
+                else
+                    show += d.str() + ", ";
+                
+                d.str("");
+            }
+            
+            s << size;
+            
+            return show + "\nTam: " + s.str();
+        }
+};
+
+void print(int *arr, int size) {
+    std::cout << "{";
+    for(int i = 0 ; i < size; i++)
+        if((i+1) != size)
+            std::cout << arr[i] << ", ";
+        else
+            std::cout << arr[i] << "}" << std::endl;
+}
+
+Heap build_min_heap(int *arr, int size) {
+    Heap h(arr, size);
+    
+    for(int i = size/2; i > 0; i--)
+        h.min_heapify(i);
+    
+    //print(h.getData(), size);
+    
+    return h;
+}
+
+void heapSort(int *arr, int size) {
+    Heap h = build_min_heap(arr, size);
+    
+    for(int i = size-1, aux; i > 0; i--) {
+        std::swap(arr[0], arr[i]);
+        
+        h.sizeDec();
+        
+        h.min_heapify(1);
+    }
+}
+
+//is min heap
+bool isHeap(int arr[], int i, int size) {
+    if(!i)
+        i++;
+    
+    int left = 2*i;
+    int right = 2*i + 1;
+    
+    if(left >= size || right >= size)
+        return true;
+    
+    if(arr[i-1] > arr[left-1] || arr[i-1] > arr[right-1])
+        return false;
+    
+    if(!isHeap(arr, left, size) || !isHeap(arr, right, size))
+        return false;
+    
+    return true;
+}
+
+#endif //HEAP_H
---
+++ Dijkstra/main.cpp
@@ -0,0 +1,109 @@
+#include "heap.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h> 
+
+#define HEAPCOMP 256000
+
+void doSort(int *, int);
+
+int main(int argc, char *argv[]) {
+    std::cout << std::endl;
+    
+    if(argc == 1) {
+        std::cout << "\nArgumentos inválidos. Digite:\n\nmake args=\"nome-do-arquivo\"\nou\nmake args=\"1 2 3 4 ... n\"\n" << std::endl;
+        return -1;
+    }
+    
+    std::stringstream *d;
+    
+    if(argc > 2) {
+        int arr[argc-1];
+        
+        for(int i = 1; i < argc; i++) {
+            d = new std::stringstream(argv[i]);
+            *d >> arr[i-1];
+        }
+        
+        //doSort(arr, (argc-1));
+    
+        Heap h = build_min_heap(arr, (argc-1));
+        
+        h.decrease_key(8, 5);
+        
+        return 1;
+    }
+    
+    /*
+    char arq[] = "files/";
+    strcat(arq, argv[1]);
+    
+    FILE *f = fopen(arq, "r");
+    
+    if(!f) {
+        std::cerr << "Arquivo não pôde ser aberto" << std::endl;
+        return 0;
+    }
+    
+    char s[1024];
+    int arr[HEAPCOMP];
+    int size = 0;
+    
+    while(true) {
+        fgets(s, 1024, f);
+    
+        if(!s) {
+            std::cerr << "Linha não pôde ser lida" << std::endl;
+            return 0;
+        }
+    
+        d = new std::stringstream(s);
+        *d >> arr[size++];
+        
+        if(feof(f))
+            break;
+    }
+    
+    fclose(f);
+    
+    doSort(arr, size);
+    
+    char sorted[] = "files/sorted-";
+    strcat(sorted, argv[1]);
+    
+    f = fopen(sorted, "w");
+    
+    if(!f) {
+        std::cerr << "Arquivo não pôde ser criado" << std::endl;
+        return 0;
+    }
+    
+    char aux[50];
+    
+    for(int i = 0; i < size; i++) {
+        sprintf(aux, "%d\n" , arr[i]);
+        
+        fputs(aux, f);
+    }
+    
+    fclose(f);
+    
+    std::cout << "Arquivo ordenado gerado." << std::endl;   
+    */
+    
+    return 0;
+}
+
+void doSort(int *arr, int size) {
+    std::cout << "\nArray...: ";
+    print(arr, size);
+        
+    std::cout << "\nHeaping...\n\nMin Heap: ";
+        
+    heapSort(arr, size);
+    
+    std::cout << "\nHeapSorting...\n\nSorted..: ";
+    print(arr, size);
+
+    std::cout << std::endl;
+}
